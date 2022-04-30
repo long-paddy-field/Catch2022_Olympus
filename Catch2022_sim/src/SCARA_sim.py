6ba647be2e4999490272f914c0c3496609c80614
long-paddy-field <4272166+long-paddy-field@users.noreply.github.com>
2022-04-30 19:26:52 +0900

    サブスクライブするようにした

--- Catch2022_sim/src/SCARA_sim.py
+++ Catch2022_sim/src/SCARA_sim.py
@@ -1,15 +1,26 @@
 from rosdep2 import RosdepInternalError
 import rospy
 from sensor_msgs.msg import JointState
+from std_msgs.msg import Float32MultiArray
 import math
 
+rviz_cmd = Float32MultiArray()
+
+def rviz_callback(data):
+    global rviz_cmd    
+    rviz_cmd = data
+
+
+
 def simulator():
+    global rviz_cmd
     rospy.init_node("SCARA_sim")
+    rviz_sub = rospy.Subscriber("motor_commands",Float32MultiArray,rviz_callback,queue_size=100)
     rviz_pub = rospy.Publisher("joint_states",JointState,queue_size=100)
 
     r = rospy.Rate(10)
-    rviz_msg = JointState()
 
+    rviz_msg = JointState()
     cnt = 0
     rviz_msg.name[0] = "stand_arm1"
     rviz_msg.name[1] = "arm1_arm2"
@@ -18,12 +29,7 @@ def simulator():
 
     while not rospy.is_shutdown():
         rviz_msg.header.stamp = rospy.Time.now()
-
-        rviz_msg.position[0] = float(cnt)/50
-        rviz_msg.position[1] = float(cnt)/50
-        rviz_msg.position[2] = float(cnt)/-1000
-        rviz_msg.position[3] = float(cnt)/50
-
+        rviz_msg.position = rviz_cmd     
         rviz_pub.publish(rviz_msg)
         cnt = cnt + 1
         r.sleep()
