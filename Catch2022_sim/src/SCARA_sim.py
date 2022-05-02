bfaac8c0c7a53147ab6da55b8ea610d1480c9f69
long-paddy-field <4272166+long-paddy-field@users.noreply.github.com>
2022-05-02 23:16:24 +0900

    わからん

--- Catch2022_sim/src/SCARA_sim.py
+++ Catch2022_sim/src/SCARA_sim.py
@@ -1,10 +1,13 @@
+from numpy import float64
 from rosdep2 import RosdepInternalError
 import rospy
 from sensor_msgs.msg import JointState
 from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import Float64
 import math
 
-rviz_cmd = Float32MultiArray()
+rviz_cmd=[0,0,0,0]
+
 
 def rviz_callback(data):
     global rviz_cmd    
@@ -15,13 +18,18 @@ def rviz_callback(data):
 def simulator():
     global rviz_cmd
     rospy.init_node("SCARA_sim")
-    rviz_sub = rospy.Subscriber("motor_commands",Float32MultiArray,rviz_callback,queue_size=100)
+    rospy.Subscriber("motor_commands",Float32MultiArray,rviz_callback,queue_size=100)
     rviz_pub = rospy.Publisher("joint_states",JointState,queue_size=100)
+    gazebo_arm1_pub = rospy.Publisher("my_SCARA/arm1_position_controller/command",Float64,queue_size=100)
+    gazebo_arm2_pub = rospy.Publisher("my_SCARA/arm2_position_controller/command",Float64,queue_size=100)
+    gazebo_linear_pub = rospy.Publisher("my_SCARA/linear_position_controller/command",Float64,queue_size=100)
+    gazebo_wrist_pub = rospy.Publisher("my_SCARA/wrist_position_controller/command",Float64,queue_size=100)
+    
 
     r = rospy.Rate(10)
 
     rviz_msg = JointState()
-    cnt = 0
+
     rviz_msg.name[0] = "stand_arm1"
     rviz_msg.name[1] = "arm1_arm2"
     rviz_msg.name[2] = "arm2_linear"
@@ -29,9 +37,9 @@ def simulator():
 
     while not rospy.is_shutdown():
         rviz_msg.header.stamp = rospy.Time.now()
-        rviz_msg.position = rviz_cmd     
+        rviz_msg.position = rviz_cmd
+        rospy.loginfo("published")     
         rviz_pub.publish(rviz_msg)
-        cnt = cnt + 1
         r.sleep()
 
 if __name__ == "__main__" :
