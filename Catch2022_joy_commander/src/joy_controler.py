5889a909cce99a8058041739cfddb25dbdd4997f
long-paddy-field <84272166+long-paddy-field@users.noreply.github.com>
2022-08-16 23:09:17 +0900

    CHANGE: 緊急停止を実装。左スティック押し込みで起動。もう一度押すまで他の入力を無視

--- Catch2022_joy_commander/src/joy_controler.py
+++ Catch2022_joy_commander/src/joy_controler.py
@@ -19,6 +19,7 @@ class JOY_CONTROLER():
         rospy.loginfo("init")
         #get_params
         self.loop_rate = 10
+        
         # self.loop_rate = rospy.get_param("loop_rate")
         
         #handles
@@ -27,7 +28,8 @@ class JOY_CONTROLER():
         self.pubRotate = rospy.Publisher("manual_arm_rotate",Float32,queue_size = 1)
         self.pub_init_flag = rospy.Publisher("hard_init",Empty,queue_size = 1)
         self.pub_start_flag = rospy.Publisher("start_flag",Empty,queue_size = 1)
-        self.pub_isHandy = rospy.Publisher("isHandy",Bool,queue_size=1)
+        self.pub_emergence_flag = rospy.Publisher("emergence_cmd",Empty,queue_size = 1)
+        self.pub_isHandy = rospy.Publisher("isHandy",Bool,queue_size = 1)
         
         
         rospy.Subscriber("joy",Joy,self.joy_callback,queue_size = 1)
@@ -36,6 +38,7 @@ class JOY_CONTROLER():
         #flags
         self.init_flag = True
         self.start_flag = False
+        self.is_emergence= False
         self.isHandy = False
         self.isGrap = False
         
@@ -57,21 +60,29 @@ class JOY_CONTROLER():
         self.cross_axes = [-1*msg.axes[4],msg.axes[5]]
         self.buttons = msg.buttons
     
-        if msg.buttons[8]:# backでinit
-            rospy.loginfo("init")
-            self.pub_init_flag.publish()
+        if msg.buttons[10]:# 左スティックで緊急停止（ソフト面）
+            rospy.logwarn("emergency stop")
+            self.is_emergence = not self.is_emergence
+            self.pub_emergence_flag.publish()
         
-        if msg.buttons[9]:# startでstart
-            rospy.loginfo("start")
-            self.pub_start_flag.publish()
+        if not self.is_emergence:
+            if msg.buttons[8]:# backでinit
+                rospy.loginfo("init")
+                self.pub_init_flag.publish()
             
-        if msg.buttons[11]:# 右スティックで手動と自動切り替え
-            self.isHandy = not self.isHandy
-            self.pub_isHandy.publish(self.isHandy)
-            if self.isHandy:
-                rospy.loginfo("change auto -> hand")
-            else :
-                rospy.loginfo("change handy -> auto")
+            if msg.buttons[9]:# startでstart
+                rospy.loginfo("start")
+                self.pub_start_flag.publish()
+                            
+            
+            if msg.buttons[11]:# 右スティックで手動と自動切り替え
+                self.isHandy = not self.isHandy
+                self.pub_isHandy.publish(self.isHandy)
+                if self.isHandy:
+                    rospy.loginfo("change auto -> hand")
+                else :
+                    rospy.loginfo("change handy -> auto")
+            
                 
     def update(self):
         rospy.loginfo("enter main routine")
@@ -79,22 +90,24 @@ class JOY_CONTROLER():
             rospy.loginfo(self.left_axes)
             rospy.loginfo(self.buttons)
             
-            f_msg = Float32MultiArray(data=self.left_axes)
-            self.pubLeftAxes.publish(f_msg)
+            
+            if not self.is_emergence:               
+                f_msg = Float32MultiArray(data=self.left_axes)
+                self.pubLeftAxes.publish(f_msg)
 
-            f_msg = Float32MultiArray(data=self.cross_axes)
-            self.pubCross.publish(f_msg)
+                f_msg = Float32MultiArray(data=self.cross_axes)
+                self.pubCross.publish(f_msg)
 
-            if not len(self.buttons) == 0:
-                if self.buttons[4] and not self.buttons[5]:
-                    rospy.loginfo("Left")
-                    f_msg = Float32(data = -1 * self.rot_ratio)
-                    self.pubRotate.publish(f_msg)
-                elif self.buttons[5] and not self.buttons[4]:
-                    rospy.loginfo("Right")
-                    f_msg = Float32(data=self.rot_ratio)
-                    self.pubRotate.publish(f_msg)
-                
+                if not len(self.buttons) == 0:
+                    if self.buttons[4] and not self.buttons[5]:
+                        rospy.loginfo("Left")
+                        f_msg = Float32(data = -1 * self.rot_ratio)
+                        self.pubRotate.publish(f_msg)
+                    elif self.buttons[5] and not self.buttons[4]:
+                        rospy.loginfo("Right")
+                        f_msg = Float32(data=self.rot_ratio)
+                        self.pubRotate.publish(f_msg)
+                    
             self.r.sleep()
 
 if __name__ == '__main__':
