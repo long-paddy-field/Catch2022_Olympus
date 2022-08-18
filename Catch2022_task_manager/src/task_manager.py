eba00fbb20a76ded908bac23f94d5d25c82cdd9d
long-paddy-field <84272166+long-paddy-field@users.noreply.github.com>
2022-08-18 18:22:47 +0900

    add: task_3まで暫定版完成

--- Catch2022_task_manager/src/task_manager.py
+++ Catch2022_task_manager/src/task_manager.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 
+from email.header import Header
+from std_msgs.msg import Int8MultiArray
 from std_msgs.msg import Empty
-from geometry_msgs.msg import Pose2D
+from sensor_msgs.msg import JointState
+from trajectory_msgs.msg import JointTrajectory
 import rospy
 import smach
 import smach_ros
 
 class Task1_Init(smach.State): #諸々の初期化待機
     def __init__(self):
-        rospy.loginfo("task_manager : Task1_Init activated")
+        rospy.loginfo("task_manager : Task1_Init is activated")
         self.is_started = False
         smach.State.__init__(self,outcomes=['done'])
         rospy.Subscriber("start_flag",Empty,self.start_flag_callback,queue_size = 1)
@@ -19,27 +22,91 @@ class Task1_Init(smach.State): #諸々の初期化待機
     def execute(self, ud):
         while not rospy.is_shutdown():
             if self.is_started:
-                rospy.loginfo("task_manager : Task1_Init ended")
+                rospy.loginfo("task_manager : Task1_Init is ended")
                 return 'done'
 
 class Task2_SeekWork(smach.State): #じゃがりこ探しに行く
     def __init__(self):
-        rospy.loginfo("task_manager : Task2_SeekWork activated")
-        smach.State.__init__(self,outcomes=['done'])
+        rospy.loginfo("task_manager : Task2_SeekWork is activated")
+        smach.State.__init__(self,outcomes=['done','completed'])
         rospy.Subscriber("grab_cmd",Empty,self.grab_cmd_callback,queue_size = 1)
-        self.target_pub = rospy.Publisher("target_location",Pose2D,queue_size = 1)
+        self.target_pub = rospy.Publisher("target_location",JointState,queue_size = 1)
         self.end_flag = False
+        self.task_counter = 0
+        self.target_stand_arm1 = [0,0,0,0,0,0,0,0,0,0]
+        self.target_arm1_arm2 = [0,0,0,0,0,0,0,0,0,0]
+        self.target_arm2_linear = [0,0,0,0,0,0,0,0,0,0]
+        self.target_linear_wrist = [0,0,0,0,0,0,0,0,0,0]
         
     def grab_cmd_callback(self):
         self.end_flag = True
     
     def execute(self, ud):
+        if self.task_counter < 10 :
+            self.target_msg = JointState()
+            self.target_msg.header = Header()
+            self.target_msg.name = ['stand_arm1', 'arm1_arm2', 'arm2_linear', 'linear_wrist']
+            self.target_msg.position = [self.target_stand_arm1[self.task_counter],
+                                        self.target_arm1_arm2[self.task_counter],
+                                        self.target_arm2_linear[self.task_counter],
+                                        self.target_linear_wrist[self.task_counter]]
+            
+            self.target_pub.publish(self.target_msg)
+            self.task_counter = self.task_counter + 1
+            
+            while not rospy.is_shutdown:
+                if self.end_flag:
+                    rospy.loginfo("task_manager : Task2_SeekWork is ended")
+                    self.end_flag = False
+                    return 'done'
+        else :
+            rospy.loginfo("task_manager : all tasks completed")
+            self.task_counter = 0
+            return 'completed'
         
-        if self.end_flag:
-            rospy.loginfo("task_manager : Task2_SeekWork ended")
-            self.end_flag = False
-            return 'done'
+class Task3_GrabWork(smach.State):
+    def __init__(self):
+        rospy.loginfo("task_manager : Task3_GrabWork is activated")
+        smach.State.__init__(self, outcomes=['done'])
+        rospy.Subscriber("is_grabbed",Int8MultiArray,self.is_grabbed_callback,queue_size=1)
+        rospy.Subscriber("move_cmd",JointTrajectory,)
+        self.task_counter = 0
+        self.arm_hight = 0
+        self.jaguar_hight = 90
+        self.is_completed = False
+        self.handstate = [0,0]
         
+    def is_grabbed_callback(self,msg):
+        self.handstate = msg.data
+        if self.task_counter < 7:
+            if msg.data[0] == 1 and msg.data[1] == 1 and self.arm_hight > self.jaguar_hight:
+                self.is_completed = True
+            else :
+                self.is_completed = False
+        
+        else:
+            if (msg.data[0] == 1 or msg.data[1] == 1) and self.arm_hight > self.jaguar_hight:
+                self.is_completed = True
+            else :
+                self.is_completed = False
+    
+    def move_cmd_callback(self,msg):
+        self.arm_hight = msg.points[2]
+        
+    def execute(self, ud):
+        while not rospy.is_shutdown():
+            if self.is_completed:
+                self.task_counter = self.task_counter + 1
+                if self.task_unter > 9:
+                    self.task_counter = 0
+                return 'done'
+
+class Task4_SeekBox(smach.State):
+    def __init__(self):
+        rospy.loginfo("task_manager : Task4_SeekBox is activated")
+        smach.State.__init__(self,outcomes=['done'])
+        
+            
 def main():
     rospy.init_node("task_manager")
      
