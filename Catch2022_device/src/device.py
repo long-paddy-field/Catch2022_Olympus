016f38e18167ad0db63c7b68a50f8e1ee6da018e
long-paddy-field <84272166+long-paddy-field@users.noreply.github.com>
2022-08-28 14:47:38 +0900

    ADD: 角度-座標変換を追加

--- Catch2022_device/src/device.py
+++ Catch2022_device/src/device.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+from cmath import pi
 from email.header import Header
 from shutil import move
 import rospy
+from typing import List
 from std_msgs.msg import Float32MultiArray
 from std_msgs.msg import Float32
 from std_msgs.msg import Int32MultiArray
@@ -12,6 +14,7 @@ from std_msgs.msg import Empty
 from cobs import cobs
 import serial
 import struct
+import math
 
 port = "/dev/ttyUSB0"
 
@@ -40,6 +43,12 @@ class device():
     def emergency_callback(self,msg):
         pass
 
+    def field_color_callback(self,msg):
+        if msg.data == True:
+            self.sign = 1
+        else:
+            self.sign = -1
+    
     def setup(self):
         global port
         self.uart = serial.Serial(port, 115200)
@@ -47,6 +56,9 @@ class device():
         self.pub1 = rospy.Publisher(
             'is_grabbed', Int32MultiArray, queue_size=1)
         self.rate = rospy.Rate(100)
+        self.l1 = 0.6
+        self.l2 = 0.3
+        self.sign = 1
 
         # subscriberの宣言
         self.sub_move_cmd = rospy.Subscriber(
@@ -59,7 +71,8 @@ class device():
             'pump_state', Bool, self.pump_state_callback, queue_size=1)
         self.sub_emergency = rospy.Subscriber(
             'emergency', Empty, self.emergency_callback, queue_size=1)
-
+        self.sub_color_field = rospy.Subscriber(
+            "field_color",Bool,self.field_color_callback,queue_size=100)
         self.msg = Float32MultiArray(data=[1, 2])
 
     def loop(self):
@@ -78,11 +91,36 @@ class device():
         currentAngle.header = Header()
         currentAngle.name = ['stand_arm1', 'arm1_arm2']
         currentAngle.position = [0, 0]  # 角度換算して入れる
+        self.theta_to_cartesian([0.5,0.5])
         isGrabbed = Int32MultiArray(data=[1, 1])
         self.pub0.publish(currentAngle)
         self.pub1.publish(isGrabbed)
 
-
+    def theta_to_cartesian(self,theta:List[float]):
+        theta1 = math.pi * (theta[0] - 35) / 180
+        theta2 = math.pi * (theta[1] - 138) / 180
+        x = self.poi(self.l1 * math.cos(theta1) + self.l2*math.cos(theta1+theta2))
+        y = self.poi(self.l1 * math.sin(theta1) + self.l2*math.sin(theta1+theta2))
+        cartesian = list([x,y])
+        return cartesian
+        
+    
+    def cartesian_to_theta(self,cartesian:List[float]):
+        x = cartesian[0]
+        y = cartesian[1]
+        theta1 = self.sign*math.acos(((x**2)+(y**2)+(self.l1**2)-(self.l2**2))/(2*self.l1*math.sqrt(x**2+y**2)))
+        theta2 = math.atan((y-self.l1*math.sin(theta1))/(x-self.l1*math.cos(theta1)))-theta1
+        theta1 = theta1 * 180 / math.pi + 35
+        theta2 = theta2 * 180 / math.pi + 138
+        theta = list([theta1,theta2])
+        return theta
+    
+    def poi(self,arg:float):
+        if math.fabs(arg) < 0.001:
+            return 0
+        else :
+            return arg
+        
 if __name__ == "__main__":
     # try:
     rospy.init_node('device')
